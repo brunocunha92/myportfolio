64c59bba5981f0e415e4ab4ad6f5d215bf109e4e
unknown <brunocunha92@gmail.com>
2023-03-08 14:53:36 +0000

    Updates

---
+++ precache-manifest.9f19cb18042837cfd2708b50947cd820.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a736263ce3f8c438b7c",
+    "url": "/myportfolio/static/css/main.4519f337.chunk.css"
+  },
+  {
+    "revision": "6a736263ce3f8c438b7c",
+    "url": "/myportfolio/static/js/main.6a736263.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/myportfolio/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "5996926ea6c13d4b238a",
+    "url": "/myportfolio/static/css/2.dacb1cbc.chunk.css"
+  },
+  {
+    "revision": "5996926ea6c13d4b238a",
+    "url": "/myportfolio/static/js/2.5996926e.chunk.js"
+  },
+  {
+    "revision": "8d51ffb3dd72ae9b1255",
+    "url": "/myportfolio/static/js/runtime~main.8d51ffb3.js"
+  },
+  {
+    "revision": "dce50326f50e49b2a8b701d3fc454c85",
+    "url": "/myportfolio/static/media/logo2.dce50326.png"
+  },
+  {
+    "revision": "0f176c0ce97a996b64cb322563cc411c",
+    "url": "/myportfolio/index.html"
+  }
+];
